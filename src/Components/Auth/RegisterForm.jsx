1fd3ecff02cd2d6ae81be0b9cb462ea854287131
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-14 10:06:47 +0300

    Updated redirection from registration to dashboard.

--- src/Components/Auth/RegisterForm.jsx
+++ src/Components/Auth/RegisterForm.jsx
@@ -24,7 +24,7 @@ const RegisterForm = () => {
         createdAt: new Date().toISOString(),
       });
 
-      navigate("/login");
+      navigate("/dashboard");
     } catch (err) {
       setError(err.message);
     }

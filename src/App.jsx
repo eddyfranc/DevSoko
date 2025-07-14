893769b622afcd7c421444b4f3c8222c4c184abc
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-14 09:35:42 +0300

    Removed duplicate Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Checkout from "./Pages/Checkout";
 function App() {
   return (
     <>
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />

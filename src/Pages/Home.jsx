a04c507e45de8e6a71ce5ef1cbcb3f60fc279c4c
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-22 11:19:26 +0300

    Updated home comments

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Why DevSoko Section */}
+      
       <div className="relative z-30 bg-white py-16">
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-10">
           Why DevSoko?

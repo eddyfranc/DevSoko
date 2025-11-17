ae02ba1013c7f001ff3b94272c898d6acaccdf6f
Tom Mwambingu <[tomchom30@gmail.com]>
2025-11-17 12:41:04 +0300

    Implemented MPESA Transaction models

--- Backend/DevBackend/payments/models.py
+++ Backend/DevBackend/payments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+#Mpesa Transaction Model
+class MpesaRequest(models.Model):
+    phone_number = models.CharField(max_length=15)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    account_reference = models.CharField(max_length=50)
+    transaction = models.CharField(max_length=255)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class MpesaResponse(models.Model):
+    request = models.ForeignKey(MpesaRequest, on_delete=models.CASCADE)
+    merchant_request_id = models.CharField(max_length=255)
+    checkout_request_id = models.CharField(max_length=10)
+    response_description = models.CharField(max_length=255)
+    response_code = models.CharField(max_length=10)
+    customer_message = models.CharField(max_length=255)
+    result_code = models.CharField(max_length=10, null=True, blank=True)
+    result_desc = models.CharField(max_length=255, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)

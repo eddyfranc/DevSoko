bb38c2ac6bbd9d0049bcb408b5652315b70f163d
Tom Mwambingu <[tomchom30@gmail.com]>
2025-11-17 20:49:54 +0300

    created Serailizers file

---
+++ Backend/DevBackend/payments/serializers.py
@@ -0,0 +1,18 @@
+from .models import MpesaRequest, MpesaResponse
+
+class MpesaRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MpesaRequest
+        fields = ['phone_number', 'amount', 'account_reference', 'transaction']
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['amount'] = str(instance.amount)
+        return ret
+
+class MpesaResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MpesaResponse
+        fields = '__all__'
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        return ret

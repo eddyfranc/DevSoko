575477134c5af6fb0f1f9b8367825d09d9f7c160
Tom Mwambingu <[tomchom30@gmail.com]>
2026-01-21 19:51:02 +0300

    Intergrated Project filtaration for Backend and Frontend to user's ViewProject page

--- src/Pages/ViewProjects.jsx
+++ src/Pages/ViewProjects.jsx
@@ -20,7 +20,6 @@ const ViewProjects = () => {
     return () => clearTimeout(timer);
   }, []);
 
-
   const categories = ['All Systems', 'Frontend', 'Backend', 'Full Stack', 'Mobile Engine', 'UI Kit', 'AI/ML Script'];
 
   const filteredProjects = useMemo(() => {
